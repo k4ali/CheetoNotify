8bb9a0d1098a2b898984fa03c42d9dbbc5966d7e
v4mmpp <zebientmax007@gmail.com>
2023-09-07 20:31:10 +0400

    feat(comp/app): add component handlers

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,28 @@
     import { Ref, defineComponent, ref } from 'vue'
 
     const notificationStorage: Ref<string[]> = ref<string[]>([]);
+    const handlers: { showNotification: () => void; deleteNotification: (id: number) => void } = {
+        showNotification: () => {
+            notificationStorage.value.push("test");
+
+            const id: number = notificationStorage.value.length;
+            setTimeout(() => {
+                handlers.deleteNotification(id);
+            }, 5000)
+        },
+
+        deleteNotification: (id: number) => {
+            notificationStorage.value = notificationStorage.value.slice(1, id);
+        }
+    };
+
     export default defineComponent({
         name: "App",
-        methods: {
-
-        }
+        methods: handlers
     })
 </script>
 <template>
     <div class="notify-app">
-
+        
     </div>
 </template>
